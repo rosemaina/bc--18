fd013761c3fb384de9a41c58054e6a22392272e4
Rose <rosemaina32@gmail.com>
2017-05-09 19:38:39 +0300

    Done

--- data_type.py
+++ data_type.py
@@ -1,33 +1,67 @@
 #Test to produce the perfect function
 
 def data_type(x):
-	#Takes an input and returns a value according to that data type of input
-	    x_type =type(x)
-	
-	#Returns a string with its length
-	    if x_type == str:
-	        return len(x)
-		
-		# Returns boolean value
-	    elif x_type ==bool:
-		      return x
-		
-	#Compares a string to 100
-	    elif x_type ==int:
-	   	    if x == 100:
-			        return 'Equal to 100'
-		    elif x < 100:
-			        return 'Less than 100'
-		    else:
-			        return 'more than 100'
 
-  #Returning items in a list. in thsi case the 3rd item
-	    elif x_type ==list:
-		    try:
-			      if x[2]:
-				        return x[2]
-		    except(IndexError):
-		  	    return None
-	    else:
-		      return 'no value'
-		
+	# Takes an input and returns a value according to that data type of input
+
+	x_type = type(x)
+
+
+
+	# Returns a string with its length
+
+	if x_type == str:
+
+		return len(x)
+
+
+
+	# Returns boolean value
+
+	elif x_type == bool:
+
+		return x
+
+
+
+	# Compares a string to 100
+
+	elif x_type == int:
+
+		if x == 100:
+
+			return 'Equal to 100'
+
+		elif x < 100:
+
+			return 'Less than 100'
+
+		else:
+
+			return 'more than 100'
+
+
+
+	# Returning items in a list. in thsi case the 3rd item
+
+	elif x_type == list:
+
+		try:
+
+			if x[2]:
+
+				return x[2]
+
+		except(IndexError):
+
+			return None
+
+	else:
+
+		return 'no value'
+
+
+
+
+
+print (data_type([1, 2, 3]))
--- primenumber.py
+++ primenumber.py
@@ -1,20 +1,39 @@
 """ Demonstrates prime number function"""
 
-""" Demonstrates virtual wallet """
+def primes(N):
 
-
-class GenPrimesTests(object):
-	""" Defines prime numbers"""
-  
-    def __init__(self):
-        self.numbers= 0;
-
-    def  isprime(n
+    prime_numbers = []
 
 
 
-    def function():
-    	pass)
+def isprime(x):
+	for n in range(2, x - 1):
+		if x % n == 0:
+			return False 
+
+		else:
+			return True
+
+	for num in range(2, N + 1):
+
+		if isprime(num):
+
+		    prime_numbers.append(num)
+
+			return prime_numbers
 
 
+		if N < 0:
+			return False
 
+		elif N== 0 and N==1:
+
+		    return False
+
+		elif
+			return 'Error'
+		  
+			pass
+
+
+print primes(23)
