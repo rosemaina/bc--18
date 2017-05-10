9546ad643b52f498c3e9cf88d25f87b1adb3610f
Rose <rosemaina32@gmail.com>
2017-05-10 18:08:18 +0300

    Modified

--- http_web.py
+++ http_web.py
@@ -1,20 +1,51 @@
-
 """ Returns error message whenone does not input characters within the range of 1-50"""
-
-import json
+import requests
 
 def get_data(get_url, post_id):
-    
+
 
     try:
+
         isinstance(int(post_id), int)
+
         if int(post_id) <= 50 and int(post_id) > 0:
+
             r = requests.get(get_url + post_id)
+
             return r
+
         else:
+
             print("Enter a No between 1 and 50")
+
     except ValueError as e:
 
+
+
         print("A whole number is required for your request to succeed")
+
         raise(e)
+
     return "* Error Message*"
+
+
+
+def main():
+
+    print("Getting some data from'jsonplaceholder.typicode.com' website")
+
+    url = "https://jsonplaceholder.typicode.com/posts/"
+
+    post_id = input("Enter a No between 1 and 50: ")
+
+    print("Getting your request...")
+
+    get_r = get_data(url,post_id)
+
+    print("GET Response data Status code Headers" % ("-"*13,get_r.text, "-"*21, get_r.status_code,"-"*2, get_r.headers))
+
+
+
+if __name__ == '__main__':
+
+    main()
